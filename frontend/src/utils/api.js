1200abd0d42d70d04efcec488f1c5fe2f81db3da
tegarpratama1105 <tegar.pratama1105@gmail.com>
2026-09-25 23:45:22 +0700

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,6 @@
 
-const BASE_URL = "https://bastp-backend-api.vercel.app"; 
-//const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://bastp-backend-api.vercel.app";
+const PROVINSI_BASE_URL = "https://backendproject-silk.vercel.app";
+
+export { PROVINSI_BASE_URL };
 export default BASE_URL;
